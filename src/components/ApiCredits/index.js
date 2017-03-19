d5493bdb5121a11ff9e442b24cdb8137a363f294
Vladimir Rodkin <mail@vovanr.com>
2017-03-19 15:58:51 +0300

    Add .editorconfig; Start migration

---
+++ src/components/ApiCredits/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+const ApiCredits = () => (
+	<span className="api-credits">
+		<span className="api-credits__text">
+			weather API by
+		</span>
+
+		<span className="api-credits__logo"/>
+	</span>
+);
+
+export default ApiCredits;
